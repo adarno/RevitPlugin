7b6b7cc453634b61cdc139157e95e97a76ab1f0e
Adrian Arnold <adrian.arnold@t-online.de>
2019-03-27 14:25:32 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace UI_Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox_search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ip_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textbox_search
+            // 
+            this.textbox_search.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.textbox_search.Location = new System.Drawing.Point(70, 80);
+            this.textbox_search.Name = "textbox_search";
+            this.textbox_search.Size = new System.Drawing.Size(200, 20);
+            this.textbox_search.TabIndex = 3;
+            this.textbox_search.Enter += new System.EventHandler(this.Search_Enter);
+            this.textbox_search.Leave += new System.EventHandler(this.Search_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.label1.Location = new System.Drawing.Point(67, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NDT 2 BIM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 6;
+            // 
+            // ip_label
+            // 
+            this.ip_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ip_label.AutoSize = true;
+            this.ip_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(158)))), ((int)(((byte)(159)))));
+            this.ip_label.Location = new System.Drawing.Point(670, 27);
+            this.ip_label.Name = "ip_label";
+            this.ip_label.Size = new System.Drawing.Size(73, 26);
+            this.ip_label.TabIndex = 5;
+            this.ip_label.Text = "individualized \nproduction";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ip_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textbox_search);
+            this.Name = "Form1";
+            this.Text = "NDT 2 BIM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textbox_search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ip_label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace UI_Prototype
+{
+    public partial class Form1 : Form
+    {
+        bool matchButtonActive = false;
+        RoundedButton splitWallButton = new RoundedButton();
+        RoundedButton assignButton = new RoundedButton();
+        RoundedButton createMaterialButton = new RoundedButton();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            //code from https://www.youtube.com/watch?v=wp8j02G3MyM
+
+            Application.DoEvents();
+            RoundedButton myButton = new RoundedButton();
+            myButton.Text = "Import";
+            EventHandler myEventHandler = new EventHandler(myButton_Click);
+            myButton.Click += myEventHandler;
+            myButton.Location = new System.Drawing.Point(300, 80);
+            System.Drawing.Color color_import_button = System.Drawing.ColorTranslator.FromHtml("#0078D7");
+            myButton.BackColor = color_import_button;
+            myButton.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            myButton.Size = new System.Drawing.Size(400, 35);
+            myButton.Anchor = (AnchorStyles.Right | AnchorStyles.Top);
+            this.Controls.Add(myButton);
+        }
+
+        private void myButton_Click(Object sender, EventArgs e)
+        {
+
+            // change button size
+            Button button = sender as Button;
+            button.Size = new System.Drawing.Size(200, 35);
+
+            //grid view
+            DataGridView grid = new DataGridView();
+            grid.Location = new System.Drawing.Point(300, 300);
+            grid.ColumnCount = 1;
+            grid.Columns[0].Name = "Wall Elements";
+
+            // open file
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV|*.csv", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (TextFieldParser parser = new TextFieldParser(ofd.FileName))
+                    {
+                        // split the cells at ";"
+                        parser.TextFieldType = FieldType.Delimited;
+                        parser.SetDelimiters(";");
+
+                        while (!parser.EndOfData)
+                        {
+                            //Process row
+                            string[] fields = parser.ReadFields();
+                            foreach (string field in fields[0])
+                            {
+                                MessageBox.Show(field);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            // add match material button
+            place_match_button();
+
+            this.Controls.Add(grid);
+
+        }
+
+        private void place_match_button()
+        {
+            RoundedButton matchButton = new RoundedButton();
+            matchButton.Text = "Match Materials";
+            EventHandler myEventHandler = new EventHandler(match_button_Click);
+            matchButton.Click += myEventHandler;
+            matchButton.Location = new System.Drawing.Point(550, 80);
+            System.Drawing.Color color_import_button = System.Drawing.ColorTranslator.FromHtml("#D16900");
+            matchButton.BackColor = color_import_button;
+            matchButton.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            matchButton.Size = new System.Drawing.Size(200, 35);
+            matchButton.Anchor = (AnchorStyles.Right | AnchorStyles.Top);
+            this.Controls.Add(matchButton);
+        }
+
+        private void match_button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (this.matchButtonActive)
+            {
+                // hide sub button
+                this.assignButton.Hide();
+                this.splitWallButton.Hide();
+                this.createMaterialButton.Hide();
+
+                this.matchButtonActive = false;
+            }
+            else
+            {
+                place_sub_buttons();
+                this.matchButtonActive = true;
+            }
+            
+            
+        }
+
+        private void place_sub_buttons()
+        {
+            System.Drawing.Color color_sub_button = System.Drawing.ColorTranslator.FromHtml("#9C9E9F");
+            // split wall button
+            this.splitWallButton = new RoundedButton();
+            splitWallButton.Text = "Split Wall";
+            EventHandler splitEventHandler = new EventHandler(myButton_Click);
+            splitWallButton.Click += splitEventHandler;
+            splitWallButton.Location = new System.Drawing.Point(70, 140);
+            splitWallButton.BackColor = color_sub_button;
+            splitWallButton.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            splitWallButton.Size = new System.Drawing.Size(200, 35);
+            splitWallButton.Anchor = (AnchorStyles.Right | AnchorStyles.Top);
+            this.Controls.Add(splitWallButton);
+
+            // assign manually button
+            this.assignButton = new RoundedButton();
+            assignButton.Text = "Assign Manually";
+            EventHandler assignEventHandler = new EventHandler(myButton_Click);
+            assignButton.Click += assignEventHandler;
+            assignButton.Location = new System.Drawing.Point(300, 140);
+            assignButton.BackColor = color_sub_button;
+            assignButton.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            assignButton.Size = new System.Drawing.Size(200, 35);
+            assignButton.Anchor = (AnchorStyles.Right | AnchorStyles.Top);
+            this.Controls.Add(assignButton);
+
+            // create material
+            this.createMaterialButton = new RoundedButton();
+            createMaterialButton.Text = "Create Material";
+            EventHandler createEventHandler = new EventHandler(myButton_Click);
+            createMaterialButton.Click += createEventHandler;
+            createMaterialButton.Location = new System.Drawing.Point(550, 140);
+            createMaterialButton.BackColor = color_sub_button;
+            createMaterialButton.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            createMaterialButton.Size = new System.Drawing.Size(200, 35);
+            createMaterialButton.Anchor = (AnchorStyles.Right | AnchorStyles.Top);
+            this.Controls.Add(createMaterialButton);
+        }
+
+        private void Search_Enter(object sender, EventArgs e)
+        {
+            if (textbox_search.Text == "Search")
+            {
+                textbox_search.Text = "";
+                textbox_search.ForeColor = Color.Black;
+            }
+        }
+
+        private void Search_Leave(object sender, EventArgs e)
+        {
+            if (textbox_search.Text == "")
+            {
+                textbox_search.Text = "Search";
+                textbox_search.ForeColor = Color.Silver;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class RoundedButton : Button
+    {
+        // code from https://www.youtube.com/watch?v=wp8j02G3MyM
+        GraphicsPath GetRoundPath(RectangleF rect, int radius)
+        {
+            float r2 = radius / 2f;
+            GraphicsPath graphicsPath = new GraphicsPath();
+
+            // round each corner
+            graphicsPath.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            graphicsPath.AddLine(rect.X + r2, rect.Y, rect.Width - r2, rect.Y);
+
+            graphicsPath.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            graphicsPath.AddLine(rect.Width, rect.Y + r2, rect.Width, rect.Height - r2);
+
+            graphicsPath.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius,
+                radius, radius, 0, 90);
+            graphicsPath.AddLine(rect.Width - r2, rect.Height, rect.X + r2, rect.Height);
+
+            graphicsPath.AddArc(rect.X, rect.Y +rect.Height - radius, radius, radius, 90, 90);
+            graphicsPath.AddLine(rect.X, rect.Height - r2, rect.X, rect.Y +r2);
+
+            graphicsPath.CloseFigure();
+            return graphicsPath;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            RectangleF Rect = new RectangleF(0, 0, this.Width, this.Height);
+            GraphicsPath GraphPath = GetRoundPath(Rect, 40);
+
+            this.Region = new Region(GraphPath);
+            using (Pen pen = new Pen(Color.CadetBlue, 1.75f))
+            {
+                pen.Alignment = PenAlignment.Inset;
+                pevent.Graphics.DrawPath(pen, GraphPath);
+            }
+        }
+    }
+
+
+}
